a6e82c6fb80769de4f9d9e5308abc13eb308afad
raihanph21 <m.rhn.ph.dev@gmail.com>
2026-04-04 11:44:25 +0700

    feat: configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 import "./App.css";
+import PageNav from "./components/PageNav";
 
 function App() {
-  return;
+  return (
+    <>
+      <PageNav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />{" "}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+  return (
+    <div>
+      <p>Page not found :(</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
