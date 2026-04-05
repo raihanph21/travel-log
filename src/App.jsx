805666530a1d4d4bc34373b91495bfc0bab0d6d4
raihanph21 <m.rhn.ph.dev@gmail.com>
2026-04-05 09:00:02 +0700

    feat: builds website pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import "./App.css";
@@ -11,9 +12,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
