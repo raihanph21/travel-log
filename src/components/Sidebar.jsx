976b193ec4122b80769e3b229588c29ae5c98da4
raihanph21 <m.rhn.ph.dev@gmail.com>
2026-04-05 09:19:40 +0700

    feat: builds static layout for the app

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+import Footer from "./Footer";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of Cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
