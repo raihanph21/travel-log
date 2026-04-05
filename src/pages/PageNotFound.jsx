f06e9c538d13b7857efc30297e009661c36ee518
raihanph21 <m.rhn.ph.dev@gmail.com>
2026-04-05 08:41:25 +0700

    chore: replace placeholder files

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <p>Page not found :(</p>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
