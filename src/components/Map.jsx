7d76eeac596e3cdf32e229fda3e8c61a414faac9
raihanph21 <m.rhn.ph.dev@gmail.com>
2026-04-16 21:42:03 +0700

    feat: get data from clicked map in the URL

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,17 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
@@ -21,7 +27,7 @@ function Map() {
   );
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
         zoom={10}
@@ -43,6 +49,7 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
@@ -51,6 +58,15 @@ function Map() {
     const map = useMap();
     map.setView(position);
     return null;
+  }
+
+  function DetectClick() {
+    const navigate = useNavigate();
+    useMapEvent({
+      click: (e) => {
+        navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+      },
+    });
   }
 }
 
